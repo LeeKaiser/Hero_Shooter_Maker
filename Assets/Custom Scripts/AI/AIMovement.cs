2adb913b760cdc2113a2d80ad85bed5765cf000e
Kaiser Lee <kaiserkaiserlee@gmail.com>
2026-03-03 12:56:07 -0800

    ai now supports all existing movement styles

--- Assets/Custom Scripts/AI/AIMovement.cs
+++ Assets/Custom Scripts/AI/AIMovement.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.AI;
 using StarterAssets;
+using MovementStyles;
 
 public class AIMovement : MonoBehaviour
 {
@@ -15,46 +16,52 @@ public class AIMovement : MonoBehaviour
 
     private StarterAssetsInputs movementInput;
 
+    private Vector3 agentDirection;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         playerRef = GetComponentInParent<PlayableCharCore>();
         movementInput = GetComponent<StarterAssetsInputs>();
         agent.updateRotation = false;
+        //set agent's speed to neglegable amount that is above 0 in order to find the direction that agent should move on its pathfinding
         agent.speed = 0.01f;
     }
 
     void Update()
     {
         MoveToLocation(moveTarget.position);
-        SetLookDirection(lookTarget.position);
-        //CorrectNavMeshData();
         AgentMovement();
     }
 
+    //makes an artificial input to the agent's input file.
     public void AgentMovement()
     {
-        Vector3 directionToInput = transform.InverseTransformDirection(agent.velocity.normalized);
-        Vector2 agentDirection = new Vector2(directionToInput.normalized.x, directionToInput.normalized.z);
-        Debug.Log(agentDirection);
-        movementInput.MoveInput(agentDirection);
+        
+        switch (playerRef.movementStyle)
+        {
+            case MovementStyle.RotateInsteadOfStrafe:
+                agentDirection = transform.InverseTransformDirection(agent.velocity.normalized);
+                break;
+            default:
+                Vector3 agentLookVect = lookTarget.position - transform.position;
+                agentLookVect = agentLookVect.normalized; //direction agent is aiming at
+                Vector3 agentMoveVect = agent.velocity.normalized; //direction agent moves in world space
+                agentDirection = Quaternion.Inverse(Quaternion.LookRotation(agentLookVect)) * agentMoveVect;
+                break;
+        }
+        
+        Vector2 agentInput = new Vector2(agentDirection.normalized.x, agentDirection.normalized.z);
+        movementInput.MoveInput(agentInput);
     }
 
     
-
+    //Move To Location
+    //sets navmesh agent's destination to the location in parameter. generally called with position of moveTarget.
     public void MoveToLocation(Vector3 destination)
     {
         agent.SetDestination(destination);
     }
 
-    public void SetLookDirection(Vector3 lookPosition)
-    {
-        if (!agent.updateRotation)
-        {
-            lookPosition.y = transform.position.y;
-            Vector3 lookDirection = lookPosition - transform.position;
-            Quaternion lookRotation = Quaternion.LookRotation(lookDirection, Vector3.up);
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * agent.angularSpeed);
-        }
-    }
+    
 }
