8b8e8e2cbfe013e72996b7f2edf50dc0bbf49a7b
Kaiser Lee <kaiserkaiserlee@gmail.com>
2026-03-11 20:20:59 -0700

    reorganized AI files and added comments

--- Assets/Custom Scripts/AI/AI Action/AIAction.cs
+++ Assets/Custom Scripts/AI/AI Action/AIAction.cs
@@ -1,12 +1,24 @@
 using UnityEngine;
 
+/*
+AI Action
+abstract parent class for AI's logic for acting in the world
+determines where the player looks at, moves to, and ability use
+*/
 public abstract class AIAction : ScriptableObject
 {
+    //Variable - Public
+    //transform representing position character moves to
     public Transform movementDestination;
+    //transform representing position character aims at
     public Transform aimTarget;
+    //reference to agent's object detection
     public ObjectDetection objectDetection;
+    //reference to agent's ability input event caller
     public InputEventCaller inputCall;
 
+    //Methods
+    //initializes variables
     public void Init(Transform movement, Transform aim, ObjectDetection od, InputEventCaller input)
     {
         movementDestination = movement;
@@ -15,8 +27,11 @@ public abstract class AIAction : ScriptableObject
         inputCall = input;
     }
 
+    //sets movement destination
     public abstract void DetermineMovement();
+    //sets aim location
     public abstract void DetermineAim();
+    //makes ability inputs
     public abstract void MakeInput();
 
 }
--- Assets/Custom Scripts/AI/Decision making/DecisionTreeNode.cs
+++ Assets/Custom Scripts/AI/Decision making/DecisionTreeNode.cs
@@ -4,26 +4,49 @@ using System.Collections.Generic;
 using System.Collections;
 using DecisionCondition;
 
+/*
+Decision tree node
+node for decision tree
+used to make decisions based on if a condition is true/false in the current situation
+*/
 public class DecisionTreeNode : ScriptableObject
 {
+    //Variables - Public
+    //reference to child node when the condition is true
     public DecisionTreeNode childYesNode;
+
+    //reference to child node when the condition is false
     public DecisionTreeNode childNoNode;
+
+    //reference to all parents
     public List<DecisionTreeNode> parentNodes = new List<DecisionTreeNode>();
+
+    //the condition that is checked on. only relevant if isBehavior is false
     public decisionCondition nodeCondition;
-    //public BehaviorGraph behavior;
+
+    //associated action. only relevant if isBehavior is true
     public AIAction action;
-    public bool isBehavior = false; //if true, return the associated behavior, if false, return the child.
 
+    //if true, return the associated behavior, if false, return the child.
+    public bool isBehavior = false; 
+
+    //parameter to check the condition on. only relevant if isBehavior is false
     public float parameterFloat = 0;
-    public Vector2 editorPosition; // for visual placement
 
-    //get the behavior that the AI should use.
+    // for visual placement
+    public Vector2 editorPosition; 
+
+
+    //Methods
+    //get the AI action that the AI should use.
     public AIAction GetAction(KnownContext currentContext)
     {
+        //return its associated behavior 
         if (isBehavior)
         {
             return action;
         }
+        //otherwise, return the child's associated behavior
         else
         {
             
@@ -43,6 +66,7 @@ public class DecisionTreeNode : ScriptableObject
         }
     }
 
+    //removes from parent nodes. used before deleting itself
     public void removeFromParent()
     {
         foreach(var x in parentNodes)
