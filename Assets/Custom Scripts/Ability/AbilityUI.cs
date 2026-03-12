de0391b10906083b0a29a9668edb996868359586
Kaiser Lee <kaiserkaiserlee@gmail.com>
2026-03-11 17:54:43 -0700

    refined comment for ability scripts

--- Assets/Custom Scripts/Ability/AbilityUI.cs
+++ Assets/Custom Scripts/Ability/AbilityUI.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public abstract class AbilityUI : MonoBehaviour
 {
+    //reference to ability
     public Ability abilityRef;
+
+    //called when UI is added
     public abstract void Initialize();
 
+    //called on update
     public abstract void UpdateUI();
 }
