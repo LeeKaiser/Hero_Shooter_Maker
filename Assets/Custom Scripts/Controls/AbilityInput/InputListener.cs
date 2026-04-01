1cec934533d5dc7e7a3ed75ae9215add5792a7bf
Kaiser Lee <kaiserkaiserlee@gmail.com>
2026-04-01 14:31:33 -0700

    updated input to discern input on press and input on release
    NOT TESTED
    need to find a way to test if this input system works
    TODO: find a way to assign ability activation id and inputunit to ability manager
    find way to make custom inputunit so that it can be tied with the ability
    maybe make the character information container system and a way to load info from container to the character

--- Assets/Custom Scripts/Controls/AbilityInput/InputListener.cs
+++ Assets/Custom Scripts/Controls/AbilityInput/InputListener.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 using System.Collections.Generic;
 using System;
 using StarterAssets;
+using InputOptions;
 /*
 InputListener
 Sends player’s inputs to InputEventCaller
@@ -12,7 +13,7 @@ public class InputListener : MonoBehaviour
     
     private PlayerInput playerInput;
     private InputEventCaller inputRead;
-    protected Dictionary<InputAction, InputOptions.Input> inputDict = new Dictionary<InputAction, InputOptions.Input>();
+    protected Dictionary<InputAction, Input> inputDict = new Dictionary<InputAction, Input>();
     
     void Awake()
     {
@@ -21,43 +22,51 @@ public class InputListener : MonoBehaviour
         inputRead = GetComponent<InputEventCaller>();
 
         //TODO: figure out a better way to initialize inputDict later
-        inputDict.Add(playerInput.actions["Num[1]"],InputOptions.Input.Num1 );
-        inputDict.Add(playerInput.actions["Num[2]"],InputOptions.Input.Num2 );
-        inputDict.Add(playerInput.actions["Num[3]"],InputOptions.Input.Num3 );
-        inputDict.Add(playerInput.actions["Num[4]"],InputOptions.Input.Num4 );
-        inputDict.Add(playerInput.actions["Num[5]"],InputOptions.Input.Num5 );
-        inputDict.Add(playerInput.actions["Num[6]"],InputOptions.Input.Num6 );
-        inputDict.Add(playerInput.actions["Num[7]"],InputOptions.Input.Num7 );
-        inputDict.Add(playerInput.actions["Num[8]"],InputOptions.Input.Num8 );
-        inputDict.Add(playerInput.actions["Num[9]"],InputOptions.Input.Num9 );
+        inputDict.Add(playerInput.actions["Num[1]"],Input.Num1 );
+        inputDict.Add(playerInput.actions["Num[2]"],Input.Num2 );
+        inputDict.Add(playerInput.actions["Num[3]"],Input.Num3 );
+        inputDict.Add(playerInput.actions["Num[4]"],Input.Num4 );
+        inputDict.Add(playerInput.actions["Num[5]"],Input.Num5 );
+        inputDict.Add(playerInput.actions["Num[6]"],Input.Num6 );
+        inputDict.Add(playerInput.actions["Num[7]"],Input.Num7 );
+        inputDict.Add(playerInput.actions["Num[8]"],Input.Num8 );
+        inputDict.Add(playerInput.actions["Num[9]"],Input.Num9 );
 
-        inputDict.Add(playerInput.actions["Ability[E]"],InputOptions.Input.AbilE );
-        inputDict.Add(playerInput.actions["Ability[Q]"],InputOptions.Input.AbilQ );
-        inputDict.Add(playerInput.actions["Ability[F]"],InputOptions.Input.AbilF );
-        inputDict.Add(playerInput.actions["Ability[R]"],InputOptions.Input.AbilR );
-        inputDict.Add(playerInput.actions["Ability[Z]"],InputOptions.Input.AbilZ );
-        inputDict.Add(playerInput.actions["Ability[X]"],InputOptions.Input.AbilX );
-        inputDict.Add(playerInput.actions["Ability[C]"],InputOptions.Input.AbilC );
-        inputDict.Add(playerInput.actions["Ability[V]"],InputOptions.Input.AbilV );
+        inputDict.Add(playerInput.actions["Ability[E]"],Input.AbilE );
+        inputDict.Add(playerInput.actions["Ability[Q]"],Input.AbilQ );
+        inputDict.Add(playerInput.actions["Ability[F]"],Input.AbilF );
+        inputDict.Add(playerInput.actions["Ability[R]"],Input.AbilR );
+        inputDict.Add(playerInput.actions["Ability[Z]"],Input.AbilZ );
+        inputDict.Add(playerInput.actions["Ability[X]"],Input.AbilX );
+        inputDict.Add(playerInput.actions["Ability[C]"],Input.AbilC );
+        inputDict.Add(playerInput.actions["Ability[V]"],Input.AbilV );
 
-        inputDict.Add(playerInput.actions["Movement[Lshift]"],InputOptions.Input.MoveLShift );
-        inputDict.Add(playerInput.actions["Movement[Lctrl]"],InputOptions.Input.MoveLCtrl );
+        inputDict.Add(playerInput.actions["Movement[Lshift]"],Input.MoveLShift );
+        inputDict.Add(playerInput.actions["Movement[Lctrl]"],Input.MoveLCtrl );
 
-        inputDict.Add(playerInput.actions["Attack[L]"],InputOptions.Input.AtkL );
-        inputDict.Add(playerInput.actions["Attack[R]"],InputOptions.Input.AtkR );
+        inputDict.Add(playerInput.actions["Attack[L]"],Input.AtkL );
+        inputDict.Add(playerInput.actions["Attack[R]"],Input.AtkR );
 
-        inputDict.Add(playerInput.actions["Misc[I]"],InputOptions.Input.MiscI );
-        inputDict.Add(playerInput.actions["Misc[O]"],InputOptions.Input.MiscO );
-        inputDict.Add(playerInput.actions["Misc[M]"],InputOptions.Input.MiscM );
+        inputDict.Add(playerInput.actions["Misc[I]"],Input.MiscI );
+        inputDict.Add(playerInput.actions["Misc[O]"],Input.MiscO );
+        inputDict.Add(playerInput.actions["Misc[M]"],Input.MiscM );
     }
     
     void Update()
     {
-        foreach (KeyValuePair<InputAction, InputOptions.Input> x in inputDict)
+        foreach (KeyValuePair<InputAction, Input> x in inputDict)
         {
+            if (x.Key.WasPressedThisFrame())
+            {
+                
+            }
             if (x.Key.IsPressed())
             {
                 inputRead.AddInput(x.Value);
+            }
+            if (x.Key.WasReleasedThisFrame())
+            {
+                
             }
         }
     }
--- Assets/Custom Scripts/Controls/AbilityInput/InputOptions.cs
+++ Assets/Custom Scripts/Controls/AbilityInput/InputOptions.cs
@@ -1,40 +1,76 @@
 using UnityEngine;
+using System.Collections.Generic;
 
-public static class InputOptions
+
+
+namespace InputOptions
 {
-    public enum Input
+    public enum InputEnum
     {
-        Num1,
-        Num2,
-        Num3,
-        Num4,
-        Num5,
-        Num6,
-        Num7,
-        Num8,
-        Num9,
+        Num1 = 1 << 0,
+        Num2 = 1 << 1,
+        Num3 = 1 << 2,
+        Num4 = 1 << 3,
+        Num5 = 1 << 4,
+        Num6 = 1 << 5,
+        Num7 = 1 << 6,
+        Num8 = 1 << 7,
+        Num9 = 1 << 8,
 
-        AbilE,
-        AbilQ,
-        AbilF,
-        AbilR,
-        AbilZ,
-        AbilX,
-        AbilC,
-        AbilV,
+        AbilE = 1 << 9,
+        AbilQ = 1 << 10,
+        AbilF = 1 << 11,
+        AbilR = 1 << 12,
+        AbilZ = 1 << 13,
+        AbilX = 1 << 14,
+        AbilC = 1 << 15,
+        AbilV = 1 << 16,
 
-        MoveLShift,
-        MoveLCtrl,
+        MoveLShift = 1 << 17,
+        MoveLCtrl = 1 << 18,
 
-        AtkL,
-        AtkR,
+        AtkL = 1 << 19,
+        AtkR = 1 << 20,
 
-        MiscI,
-        MiscO,
-        MiscM,
-
-        Jump
+        MiscI = 1 << 21,
+        MiscO = 1 << 22,
+        MiscM = 1 << 23,
     }
+
+    public enum InputType
+    {
+        Press = 1 << 0,
+        Hold = 1 << 1,
+        Release = 1 << 2
+    }
+
+    public struct InputUnit
+    {
+        public Input InputCombo;
+        public InputType ComboInputType;
+
+        //CompareInputToCombo
+        //compares input to the InputCombo. return true if the input matches the combo and therefore should activate.
+
+        public bool CompareInputToCombo(InputEnum pressInputs, InputEnum holdInputs, InputEnum releaseInputs)
+        {
+            switch (ComboInputType)
+            {
+                case InputType.Press:
+                    //TODO: would be very sensitive so find a way to make it more lenient if it is an issue
+                    return InputCombo & pressInputs > InputCombo;
+                case InputType.Hold:
+                    return InputCombo & holdInputs > InputCombo;
+                case InputType.Release:
+                    return InputCombo & releaseInputs > InputCombo;
+                default:
+                    return false;
+            }
+        }
+    }       
 }
 
 public class ActiveAbilityID{}
+
+
+
