4ec88619e93d0db829ac0c3c7ce7c167e251edb1
Kaiser Lee <kaiserkaiserlee@gmail.com>
2026-04-15 13:50:27 -0700

    now has support for healing number done to allies

--- Assets/Custom Scripts/PlayerClient/ClientDamageNumber.cs
+++ Assets/Custom Scripts/PlayerClient/ClientDamageNumber.cs
@@ -8,8 +8,11 @@ public class ClientDamageNumber : MonoBehaviour
     public CharCore CharacterReference;
     public Transform PlayerCamera;
     public GameObject DamageNumberPrefab;
+    public GameObject HealNumberPrefab;
 
     Dictionary<CharCore, int> characterToDamage = new Dictionary<CharCore, int>();
+
+    Dictionary<CharCore, int> characterToHeal = new Dictionary<CharCore, int>();
     int recentDamageTaken = 0;
 
     IEnumerator GenerateDamageNumber()
@@ -32,6 +35,26 @@ public class ClientDamageNumber : MonoBehaviour
         
     }
 
+    IEnumerator GenerateHealNumber()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.2f);
+            
+            foreach (KeyValuePair<CharCore, int> character in characterToHeal)
+            {
+                Vector3 damageNumPos = character.Key.PlayerArmature.transform.position;
+                damageNumPos.y += character.Key.PlayerArmature.GetComponent<CharacterController>().height;
+                damageNumPos += new Vector3(Random.Range(-0.3f,0.3f),Random.Range(-0.3f,0.3f),Random.Range(-0.3f,0.3f));
+                GameObject damageNoVis = Instantiate(HealNumberPrefab, damageNumPos , Quaternion.identity);
+                damageNoVis.GetComponent<DamageNumberScript>().Init(PlayerCamera, ""+character.Value);
+            }
+            
+            characterToHeal.Clear();
+        }
+    }
+
+    //TODO: change to make it show multiple damage number for different sources
     IEnumerator GenerateDamageTakenNumber()
     {
         while (true)
@@ -70,6 +93,22 @@ public class ClientDamageNumber : MonoBehaviour
         
     }
 
+    public void ShowHealingDone(PlayerHealHealth healHealth)
+    {
+        if (healHealth.Healer != CharacterReference)
+        {
+            return;
+        }
+        if (characterToHeal.ContainsKey(healHealth.PlayerIdentity))
+        {
+            characterToHeal[healHealth.PlayerIdentity] += healHealth.Healing;
+        }
+        else
+        {
+            characterToHeal.Add(healHealth.PlayerIdentity, healHealth.Healing);
+        }
+    }
+
     public void ShowTakenDamage(PlayerTakeDamage takeDamage)
     {
         if (takeDamage.PlayerIdentity != CharacterReference)
@@ -84,13 +123,16 @@ public class ClientDamageNumber : MonoBehaviour
     {
         EventBus<PlayerTakeDamage>.Subscribe(ShowDamageDealt);
         EventBus<PlayerTakeDamage>.Subscribe(ShowTakenDamage);
+        EventBus<PlayerHealHealth>.Subscribe(ShowHealingDone);
         StartCoroutine(GenerateDamageNumber());
         StartCoroutine(GenerateDamageTakenNumber());
+        StartCoroutine(GenerateHealNumber());
     }
 
     void OnDisable()
     {
         EventBus<PlayerTakeDamage>.Unsubscribe(ShowDamageDealt);
         EventBus<PlayerTakeDamage>.Unsubscribe(ShowTakenDamage);
+        EventBus<PlayerHealHealth>.Unsubscribe(ShowHealingDone);
     }
 }
