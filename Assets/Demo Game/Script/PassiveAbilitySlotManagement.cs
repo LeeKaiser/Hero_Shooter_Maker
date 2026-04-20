894f420fde585725366b2fbe49e967a72e52052d
Kaiser Lee <kaiserkaiserlee@gmail.com>
2026-04-20 16:47:52 -0700

    Added heal teammate passive

--- Assets/Demo Game/Script/PassiveAbilitySlotManagement.cs
+++ Assets/Demo Game/Script/PassiveAbilitySlotManagement.cs
@@ -4,14 +4,25 @@ using System.Collections.Generic;
 public class PassiveAbilitySlotManagement : MonoBehaviour
 {
     public List<GameObject> allAbilities = new List<GameObject>();
+    [SerializeField] List<GameObject> allAbilitiesCopy = new List<GameObject>();
 
+    void Awake()
+    {
+        foreach (GameObject abil in allAbilities)
+        {
+            GameObject abilCopy = Instantiate(abil);
+            DontDestroyOnLoad(abilCopy);
+            abilCopy.SetActive(false);
+            allAbilitiesCopy.Add(abilCopy);
+        }
+    }
     public void AddToAssember(CharAssembleInfo assemble, int index)
     {
         //check if it has ability that belongs to its own slot, remove it if it finds one
         GameObject toRemove = null;
         foreach (GameObject abil in assemble.OtherAbilities)
         {
-            if (assemble.OtherAbilities.Contains(abil))
+            if (allAbilitiesCopy.Contains(abil))
             {
                 toRemove = abil;
             }
@@ -22,6 +33,8 @@ public class PassiveAbilitySlotManagement : MonoBehaviour
         }
         
         //add ability at index
-        assemble.OtherAbilities.Add(allAbilities[index]);
+        Debug.Log(index);
+        Debug.Log(allAbilities.Count);
+        assemble.OtherAbilities.Add(allAbilitiesCopy[index]);
     }
 }
